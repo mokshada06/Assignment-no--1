4b144786d794ea416607f05342c9a6b66589835a
mokshada06 <mokshadashpute24@gmail.com>
2025-05-05 11:10:57 +0530

    Add files via upload

---
+++ Assignment no 1.py
@@ -0,0 +1,30 @@
+#3 Task no 1
+
+a=int(input('enter a 1st no :'))
+b=int(input('enter a 2nd no :'))
+addition = a + b
+print(addition)
+subtraction = a - b
+print(subtraction)
+multiplication = a * b
+print(multiplication)
+division = a / b
+print(division)
+
+Output:
+enter a 1st no : 5
+enter a 2nd no : 10
+15
+-5
+50
+0.5
+
+#3 Task no 2
+x=input('enter a First Name :')
+y=input('enter a Last Name :')
+print('Hello',x,y,'Welcome to Python Programme')
+
+Output: 
+enter a First Name : Mokshada
+enter a Last Name : Dashpute!
+Hello Mokshada Dashpute! Welcome to Python Programme
